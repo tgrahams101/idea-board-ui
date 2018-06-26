4856383f88cd5b1fdbdefd955acae8c98d7f613b
Ted Anyansi <tanyansi@gmail.com>
2018-06-25 22:11:24 -0700

    added create, update, and delete functionality to idea boards

--- src/components/Idea.jsx
+++ src/components/Idea.jsx
@@ -2,13 +2,25 @@ import React, {Component} from 'react'
 
 class Idea extends Component {
 
+    handleKeyUp(event) {
+        console.log('HANDLING KEY UP', event);
+    }
     render() {
         return (
             <div>
-                <div>{this.props.title}</div>
-                <div>{this.props.description}</div>
+                <p><b>Idea: {this.props.title}</b></p>
+                <p>Description: {this.props.description}</p>
+                <button onClick={ (event) => {
+                    console.log('EVENT', event);
+                    this.props.deleteIdea(this.props.id, this.props.index);
+                }} > Delete
+                </button>
+                <h5>Change Idea details below </h5>
+                <input name="title" value={this.props.title} onChange={this.props.handleIdeaChange.bind(null, this.props.index)} onKeyUp={this.handleKeyUp} onBlur={this.props.updateIdea.bind(null, this.props.index)}/>
+                <br />
+                <textarea name="description" value={this.props.description} onChange={this.props.handleIdeaChange}/>
+
                 <hr/>
-                <button onClick={this.props.deleteIdea.bind(null, this.props.id, this.props.index)}> Delete </button>
             </div>
         )
     }
--- src/components/IdeaList.jsx
+++ src/components/IdeaList.jsx
@@ -4,6 +4,10 @@ import Idea from './Idea.jsx';
 import IdeaNewForm from './IdeaNewForm.js';
 
 class IdeaList extends Component {
+    constructor() {
+        super();
+        this.updateIdea = this.updateIdea.bind(this);
+    }
 	state = {
 		ideas: []
 	}
@@ -20,8 +24,15 @@ class IdeaList extends Component {
             });
     }
 
-    deleteIdea = async (event, ideaId, index) => {
-        console.log(event, ideaId, index);
+    componentWillUpdate(nextProps, nextState) {
+        console.log('CURRENT STATE', this.state);
+        console.log('NEXT STATE', nextState);
+        console.log('IS EQUAL', this.state.ideas[0] === nextState.ideas[0]);
+    }
+
+    deleteIdea = async (ideaId, index) => {
+        console.log('IDEA ID', ideaId);
+        console.log('INDEX', index);
         try {
             await axios.delete(`/ideas/${ideaId}`)
             
@@ -35,22 +46,44 @@ class IdeaList extends Component {
         }
     }
 
-    createIdea = async (...args) => {
-        args[1].preventDefault();
-        console.log(args, args.length);
-        console.log(...args);
-        // console.log('IDEA', idea);
-        // try {
-        //     const newIdeaResponse = await axios.post(`/ideas`, idea)
+    createIdea = async (newIdea, event) => {
+        event.preventDefault();
+        console.log('IDEA', newIdea);
+        try {
+            const newIdeaResponse = await axios.post(`/ideas`, newIdea)
+            console.log('DATA FIELD FROM POST', newIdeaResponse.data);
+            const firstObj = this.state.ideas[0];
+            const updatedIdeasList = [...this.state.ideas]
+            updatedIdeasList.push(newIdeaResponse.data)
+            this.setState({ideas: updatedIdeasList})
     
-        //     const updatedIdeasList = [...this.state.ideas]
-        //     updatedIdeasList.push(newIdeaResponse.data)
-        //     this.setState({ideas: updatedIdeasList})
-    
-        // } catch(error) {
-        //     console.log('Error creating new User!')
-        //     console.log(error)
-        // }
+        } catch(error) {
+            console.log('Error creating new User!')
+            console.log(error)
+        }
+    }
+
+    handleIdeaChange = (index, event) => {
+        console.log('INDEX', index);
+        console.log('EVENT', event);
+        const attributeToChange = event.target.name;
+        const newValue = event.target.value;
+
+        const updatedIdeasList = [...this.state.ideas];
+        const ideaToUpdate = updatedIdeasList[index];
+        ideaToUpdate[attributeToChange] = newValue;
+        this.setState({ideas: updatedIdeasList});
+    }
+
+    async updateIdea(index) {
+        try {
+            const ideaToUpdate = this.state.ideas[index];
+            let response = await axios.patch(`/ideas/${ideaToUpdate.id}`, ideaToUpdate);
+            console.log('RESPONSE FROM PATCH', response);
+            console.log('DATA FIELD FROM PATCH', response.data)
+        } catch(error) {
+            console.log('Error updating Idea!', error);
+        }
     }
 
 	render() {
@@ -60,7 +93,7 @@ class IdeaList extends Component {
             <IdeaNewForm createIdea={this.createIdea} />
                 
                 { this.state.ideas.map( (element, index) => {
-                    return <Idea {...element} key={index} index={index} deleteIdea={this.deleteIdea} createIdea={this.createIdea}/>
+                    return <Idea {...element} key={index} title={element.title} index={index} handleIdeaChange={this.handleIdeaChange} deleteIdea={this.deleteIdea} createIdea={this.createIdea} updateIdea={this.updateIdea}/>
                 })}
 	        </div>
 	    )
